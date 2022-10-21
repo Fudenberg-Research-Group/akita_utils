6089e3e65f1e038e8d11c22cb38bab392cb29712
Paulina Smaruj <smaruj@usc.edu>
2022-10-21 15:01:25 -0700

    seqs_gen() debugged, added to utils, test added to tests

--- akita_utils/utils.py
+++ akita_utils/utils.py
@@ -8,7 +8,7 @@ import glob
 from io import StringIO
 import h5py
 import random
-
+from basenji import dna_io
 
 ### numeric utilites
 def absmaxND(a, axis=None):
@@ -665,3 +665,56 @@ def split_df_equally(df, num_chunks, chunk_idx):
     ]
     
     return df_chunk
+
+
+# insertion experiments utils
+def symmertic_insertion_seqs_gen(seq_coords_df, background_seqs, genome_open, test=False):
+    """ sequence generator for making insertions from tsvs
+        construct an iterator that yields a one-hot encoded sequence
+        that can be used as input to akita via PredStreamGen
+    """
+    
+    if test:
+        seq_length = 1310720
+    
+    for s in seq_coords_df.itertuples():
+
+        flank_bp = s.flank_bp
+        spacer_bp = s.spacer_bp
+        num_inserts = len(s.orientation)
+
+        seq_1hot_insertion = dna_io.dna_1hot(
+            genome_open.fetch(s.chrom, s.start - flank_bp, s.end + flank_bp).upper()
+        )
+        if s.strand == "-":
+            seq_1hot_insertion = dna_io.hot1_rc(seq_1hot_insertion)
+            # now, all motifs are standarized to this orientation ">"
+
+        seq_1hot = background_seqs[s.background_index].copy()
+        insert_bp = len(seq_1hot_insertion)
+        insert_plus_spacer_bp = insert_bp + 2 * spacer_bp
+        multi_insert_bp = num_inserts * insert_plus_spacer_bp
+        insert_start_bp = seq_length // 2 - multi_insert_bp // 2
+
+        insertion_starting_positions = []
+
+        for i in range(num_inserts):
+            offset = insert_start_bp + i * insert_plus_spacer_bp + spacer_bp
+
+            insertion_starting_positions.append(offset)
+
+            for orientation_arrow in s.orientation[i]:
+                if orientation_arrow == ">":
+                    seq_1hot[offset : offset + insert_bp] = seq_1hot_insertion
+                else:
+                    seq_1hot[offset : offset + insert_bp] = dna_io.hot1_rc(seq_1hot_insertion)
+
+        if test:
+            yield insertion_starting_positions
+        else:
+            yield seq_1hot
+
+            
+            
+            
+
