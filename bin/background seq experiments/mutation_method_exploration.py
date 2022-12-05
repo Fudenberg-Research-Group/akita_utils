aaad3fbe80fc88a864b0b6e6d13ae0f45dca9f7f
Kamulegeya-Fahad <kamulege@discovery1.usc.edu>
2022-12-05 14:42:50 -0800

    few modifications

--- bin/background seq experiments/mutation_method_exploration.py
+++ bin/background seq experiments/mutation_method_exploration.py
@@ -37,7 +37,7 @@ from basenji import dna_io
 
 #from basenji import vcf as bvcf
 
-import akita_utils
+import akita_utils.background_utils import mutation_search
 from io import StringIO
 '''
 creating flat maps/seqs
@@ -201,134 +201,7 @@ def main():
   #################################################################
   # calculating initial scores
     
-  def mutation_search(seqnn_model,
-                    genome_fasta,
-                    seq_length,
-                    dataframe,
-                    max_iters=1,
-                    batch_size=6,
-                    shuffle_k=8,
-                    ctcf_thresh=8,
-                    scores_thresh = 5500,
-                    scores_pixelwise_thresh = .04,
-                    masking=False,
-                    timing = False
-                    ):
-    
-    """
-    This function creates flat sequences
-    """
-    flat_seqs = []
-    scores_storage = {}
-    num_seqs = dataframe.shape[0]
-
-    mot = ">CCAsyAGrkGGCr\n0.0000\t1.0000\t0.0000\t0.0000\n0.0000\t1.0000\t0.0000\t0.0000\n1.0000\t0.0000\t0.0000\t0.0000\n0.0000\t0.5000\t0.5000\t0.0000\n0.0000\t0.5000\t0.0000\t0.5000\n1.0000\t0.0000\t0.0000\t0.0000\n0.0000\t0.0000\t1.0000\t0.0000\n0.5000\t0.0000\t0.5000\t0.0000\n0.0000\t0.0000\t0.5000\t0.5000\n0.0000\t0.0000\t1.0000\t0.0000\n0.0000\t0.0000\t1.0000\t0.0000\n0.0000\t1.0000\t0.0000\t0.0000\n0.5000\t0.0000\t0.5000\t0.0000"
-    motif = pd.read_csv(StringIO(mot), sep="\t", header=0, names=["A", "C", "G", "T"]).values
-    motif_window = int(np.ceil(len(motif) / 2))
-    mot_shuf = [12, 0, 1, 11, 10, 3, 2, 8, 9, 4, 5, 7, 6, 13]
-
-    for ind in range(num_seqs):
-        chrom, start, end, gc = dataframe.iloc[ind][["chrom", "start", "end", "GC"]]
-        genome_open = pysam.Fastafile(genome_fasta)
-        seq = genome_open.fetch(chrom, start, end).upper()
-        seq_1hot = dna_io.dna_1hot(seq)
-
-        t0 = time.time()
-        num_iters = 0
-        while num_iters < max_iters:
-            name = f'{num_iters}'
-            # scores_set = []
-            print("ind",ind,", iter ",num_iters,", k ",shuffle_k,", for", chrom, start, end,)
-            seq_1hot_batch = []
-            for i in range(batch_size):
-                seq_1hot_mut = akita_utils.permute_seq_k(seq_1hot, k=shuffle_k)
-                s = akita_utils.scan_motif(seq_1hot_mut, motif)
-                if masking == 0:
-                    for i in np.where(s > ctcf_thresh)[0]:
-                        if len(seq_1hot_mut[i-motif_window:i+motif_window]) == len(mot_shuf):
-                            seq_1hot_mut[i-motif_window:i+motif_window] = akita_utils.permute_seq_k(seq_1hot_mut[i-motif_window:i+motif_window], k=2)
-                    seq_1hot_batch.append(seq_1hot_mut)
-                elif masking == 1:
-                    for i in np.where(s > ctcf_thresh)[0]:
-                        if len(seq_1hot_mut[i-motif_window:i+motif_window]) == len(mot_shuf):
-                            seq_1hot_mut[i-motif_window:i+motif_window] = seq_1hot_mut[i-motif_window:i+motif_window][mot_shuf]
-
-                    seq_1hot_batch.append(seq_1hot_mut)
-                else:
-                    seq_1hot_batch.append(seq_1hot_mut)
-
-            seq_1hot_batch = np.array(seq_1hot_batch)
-            pred = seqnn_model.predict(seq_1hot_batch, batch_size=batch_size)
-            scores = np.sum(pred**2, axis=-1).sum(axis=-1)
-            scores_pixelwise = np.max(pred**2, axis=-1).max(axis=-1)  
-            t1 = time.time()
-            # scores_set += [scores]
-            scores_storage[name] = [scores]
-            
-            if np.any([
-                (np.min(scores) < scores_thresh)
-                , (np.min(scores_pixelwise) < scores_pixelwise_thresh)]
-            ):
-                # scores_set += [scores]
-                best_ind = np.argmin(scores_pixelwise)
-                best_seq = seq_1hot_batch[best_ind]
-                best_pred = pred[best_ind]
-                best_score, best_score_pixelwise = (
-                    scores[best_ind],
-                    scores_pixelwise[best_ind],
-                )
-                
-                print(
-                    "success: best seq, thresh",
-                    np.min(scores),
-                    " pixelwise",
-                    np.min(scores_pixelwise),
-                    "time",
-                    t1 - t0
-                )
-                if timing == True:
-                    num_iters = max_iters
-            else:
-                best_ind = np.argmin(scores_pixelwise)
-                best_seq = seq_1hot_batch[best_ind]
-                best_pred = pred[best_ind]
-                best_score, best_score_pixelwise = (
-                    scores[best_ind],
-                    scores_pixelwise[best_ind],
-                )
-                print(
-                "trying: best seq, thresh",
-                np.min(scores),
-                " pixelwise",
-                np.min(scores_pixelwise),
-                )
-                
-            num_iters += 1
-            if num_iters >= max_iters:
-                # scores_set += [scores]
-                print(f"max iters exceeded, final time {t1 - t0}")
-                if gc :
-                    flat_seqs.append([
-                    best_seq,
-                    best_pred,
-                    best_score,
-                    best_score_pixelwise,
-                    t1 - t0,
-                    gc
-                ])
-                else:
-                    flat_seqs.append([
-                    best_seq,
-                    best_pred,
-                    best_score,
-                    best_score_pixelwise,
-                    t1 - t0
-                ])
-
-
-    return scores_storage, flat_seqs
-
-  scores_storage_random_masking, flat_seqs_random_masking = mutation_search( seqnn_model=seqnn_model,
+  scores_storage_random_masking, flat_pred_time_random_masking = mutation_search( seqnn_model=seqnn_model,
                                                                             genome_fasta=options.genome_fasta,
                                                                             seq_length=seq_length,
                                                                             dataframe= new_dataframe,
@@ -338,7 +211,7 @@ def main():
                                                                             timing = True)
   print('first expt done')
 
-  scores_storage_motif_masking, flat_seqs_motif_masking = mutation_search( seqnn_model=seqnn_model,
+  scores_storage_motif_masking, flat_pred_time_motif_masking = mutation_search( seqnn_model=seqnn_model,
                                                                             genome_fasta=options.genome_fasta,
                                                                             seq_length=seq_length,
                                                                             dataframe= new_dataframe,
@@ -348,7 +221,7 @@ def main():
                                                                             timing = True)
 
 
-  scores_storage_all_random, flat_seqs_no_mask = mutation_search( seqnn_model=seqnn_model,
+  scores_storage_all_random,flat_pred_time_no_mask = mutation_search( seqnn_model=seqnn_model,
                                                                 genome_fasta=options.genome_fasta,
                                                                 seq_length=seq_length,
                                                                 dataframe= new_dataframe,
@@ -399,15 +272,6 @@ def main():
     plt.close()
     
     # ------------------------------------------------------------------------
-    
-    flat_pred_time_random_masking = []
-    flat_pred_time_motif_masking = []
-    flat_pred_time_no_mask = []
-
-    for i in range(len(sample_set)):
-        flat_pred_time_no_mask += [flat_seqs_no_mask[i][4]]
-        flat_pred_time_random_masking += [flat_seqs_random_masking[i][4]]
-        flat_pred_time_motif_masking += [flat_seqs_motif_masking[i][4]]
 
     kde_1 = pd.DataFrame(flat_pred_time_no_mask, columns=["success time (s)"])  
     kde_2 = pd.DataFrame(flat_pred_time_motif_masking, columns=["success time (s)"])
