7bff97bdfb4ac28aa3c20e9f6a0811a050170ffa
Paulina Smaruj <smaruj@usc.edu>
2022-12-08 13:42:20 -0800

    two-insertion test for the _insert_casette() function

--- tests/test_seq_gens.py
+++ tests/test_seq_gens.py
@@ -52,3 +52,24 @@ def test_insert_casette():
         )
         == "AAA" + dna_seq_rc(motif2) + "AA"
     )
+
+    # third test (two casettes)
+    third_flank_bp = 0
+    third_spacer_bp = 0
+    third_orientation_string = "><"
+    third_seq_1hot_insertion = toy_genome_open[
+        (2 - third_flank_bp) : (6 + third_flank_bp)
+    ]
+    # toy_genome_open[(2 - third_flank_bp) : (6 + third_flank_bp)] corresponds to motif1
+
+    assert (
+        dna_1hot_to_seq(
+            _insert_casette(
+                background_1hot,
+                third_seq_1hot_insertion,
+                third_spacer_bp,
+                third_orientation_string,
+            )
+        )
+        == "A" + motif1 + dna_seq_rc(motif1) + "A"
+    )
