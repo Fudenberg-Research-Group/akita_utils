19910b87339d3959073f657f9d4c04f3f52435ea
Paulina Smaruj <smaruj@usc.edu>
2022-12-15 12:17:15 -0800

    new seq_gen for the fixed-core experiment

--- akita_utils/seq_gens.py
+++ akita_utils/seq_gens.py
@@ -1,9 +1,18 @@
 from akita_utils.dna_utils import hot1_rc, dna_1hot
 import numpy as np
 import akita_utils.format_io
+
 ########################################
 #           insertion utils            #
 ########################################
+
+
+def split_concat_to_integers(concat_string):
+    return [int(i) for i in concat_string.split(",")]
+
+
+def split_concat_to_strings(concat_string):
+    return [i for i in concat_string.split(",")]
 
 
 def _insert_casette(
@@ -36,29 +45,41 @@ def _insert_casette(
     return output_seq
 
 
-def _multi_insert_casette(seq_1hot, seq_1hot_insertions, spacer_bp, orientation_string):
-        
-    assert len(seq_1hot_insertions)==len(orientation_string), "insertions dont match orientations, please check"
+def _multi_insert_casette(
+    seq_1hot, seq_1hot_insertions, spacer_bp, orientation_string
+):
+
+    assert len(seq_1hot_insertions) == len(
+        orientation_string
+    ), "insertions dont match orientations, please check"
     seq_length = seq_1hot.shape[0]
-    total_insert_bp = sum([len(insertion) for insertion in seq_1hot_insertions])
+    total_insert_bp = sum(
+        [len(insertion) for insertion in seq_1hot_insertions]
+    )
     num_inserts = len(seq_1hot_insertions)
-    inserts_plus_spacer_bp = total_insert_bp + (2 * spacer_bp)*num_inserts
+    inserts_plus_spacer_bp = total_insert_bp + (2 * spacer_bp) * num_inserts
     insert_start_bp = seq_length // 2 - inserts_plus_spacer_bp // 2
     output_seq = seq_1hot.copy()
-    
+
     length_of_previous_insert = 0
     for i in range(num_inserts):
         insert_bp = len(seq_1hot_insertions[i])
         orientation_arrow = orientation_string[i]
-        offset = insert_start_bp + length_of_previous_insert + spacer_bp # i * inserts_plus_spacer_bp + spacer_bp
-        length_of_previous_insert += len(seq_1hot_insertions[i]) + 2*spacer_bp
+        offset = (
+            insert_start_bp + length_of_previous_insert + spacer_bp
+        )  # i * inserts_plus_spacer_bp + spacer_bp
+        length_of_previous_insert += (
+            len(seq_1hot_insertions[i]) + 2 * spacer_bp
+        )
         if orientation_arrow == ">":
             output_seq[offset : offset + insert_bp] = seq_1hot_insertions[i]
         else:
-            output_seq[offset : offset + insert_bp] = akita_utils.dna_utils.hot1_rc(seq_1hot_insertions[i])
+            output_seq[
+                offset : offset + insert_bp
+            ] = akita_utils.dna_utils.hot1_rc(seq_1hot_insertions[i])
     return output_seq
 
-        
+
 def symmertic_insertion_seqs_gen(seq_coords_df, background_seqs, genome_open):
     """sequence generator for making insertions from tsvs
     construct an iterator that yields a one-hot encoded sequence
@@ -82,74 +103,136 @@ def symmertic_insertion_seqs_gen(seq_coords_df, background_seqs, genome_open):
             # now, all motifs are standarized to this orientation ">"
 
         seq_1hot = background_seqs[s.background_index].copy()
-        
-        seq_1hot = _insert_casette(seq_1hot, seq_1hot_insertion, spacer_bp, orientation_string)
-        
+
+        seq_1hot = _insert_casette(
+            seq_1hot, seq_1hot_insertion, spacer_bp, orientation_string
+        )
+
         yield seq_1hot
-        
+
+
+def fixed_core_insertion_seqs_gen(seq_coords_df, background_seqs, genome_open):
+
+    for s in seq_coords_df.itertuples():
+
+        flank_bp = s.flank_bp
+        spacer_bp = s.spacer_bp
+        orientation_string = s.orientation
+
+        core_chrom, flank_chrom = split_concat_to_strings(s.chrom)
+        core_start, flank_start = split_concat_to_integers(s.start)
+        core_end, flank_end = split_concat_to_integers(s.end)
+        core_strand, flank_strand = split_concat_to_strings(s.strand)
+
+        # getting flank motif
+        seq_1hot_flank = dna_1hot(
+            genome_open.fetch(
+                flank_chrom, flank_start - flank_bp, flank_end + flank_bp
+            ).upper()
+        )
+        if flank_strand == "-":
+            seq_1hot_flank = hot1_rc(seq_1hot_flank)
+
+        seq_1hot_flank_left = seq_1hot_flank[:flank_bp, :]
+        seq_1hot_flank_right = seq_1hot_flank[-flank_bp:, :]
+
+        # getting core motif
+        seq_1hot_core = dna_1hot(
+            genome_open.fetch(core_chrom, core_start, core_end).upper()
+        )
+        if core_strand == "-":
+            seq_1hot_core = hot1_rc(seq_1hot_core)
+
+        # joining all the chunks together
+        seq_1hot_insertion = np.concatenate(
+            (seq_1hot_flank_left, seq_1hot_core, seq_1hot_flank_right), axis=0
+        )
+
+        seq_1hot = background_seqs[s.background_index].copy()
+
+        seq_1hot = _insert_casette(
+            seq_1hot, seq_1hot_insertion, spacer_bp, orientation_string
+        )
+
+        yield seq_1hot
+
 
 def modular_insertion_seqs_gen(seq_coords_df, background_seqs, genome_open):
     """ sequence generator for making modular insertions from tsvs
         construct an iterator that yields a one-hot encoded sequence
         that can be used as input to akita via PredStreamGen
     """
-    
+
     for s in seq_coords_df.itertuples():
         seq_1hot_insertions = []
         spacer_bp = s.spacer_bp
         orientation_string = s.locus_orientation
-        seq_1hot = background_seqs[s.background_seqs].copy()        
+        seq_1hot = background_seqs[s.background_seqs].copy()
 
         for module_number in range(len(s.insert_strand.split("$"))):
             locus = s.insert_loci.split("$")[module_number]
             flank_bp = int(s.insert_flank_bp.split("$")[module_number])
-            chrom,start,end = locus.split(",")
-            seq_1hot_insertion = akita_utils.dna_utils.dna_1hot(genome_open.fetch(chrom, int(start) - flank_bp, int(end) + flank_bp).upper())
+            chrom, start, end = locus.split(",")
+            seq_1hot_insertion = akita_utils.dna_utils.dna_1hot(
+                genome_open.fetch(
+                    chrom, int(start) - flank_bp, int(end) + flank_bp
+                ).upper()
+            )
             if s.insert_strand.split("$")[module_number] == "-":
-                seq_1hot_insertion = akita_utils.dna_utils.hot1_rc(seq_1hot_insertion)
+                seq_1hot_insertion = akita_utils.dna_utils.hot1_rc(
+                    seq_1hot_insertion
+                )
             seq_1hot_insertions.append(seq_1hot_insertion)
 
-        seq_1hot = _multi_insert_casette(seq_1hot, seq_1hot_insertions, spacer_bp, orientation_string)
+        seq_1hot = _multi_insert_casette(
+            seq_1hot, seq_1hot_insertions, spacer_bp, orientation_string
+        )
 
         yield seq_1hot
-        
-        
 
 
 # define sequence generator
 def generate_spans_start_positions(seq_1hot, motif, threshold):
     index_scores_array = akita_utils.dna_utils.scan_motif(seq_1hot, motif)
     motif_window = len(motif)
-    half_motif_window = int(np.ceil(motif_window/2))
+    half_motif_window = int(np.ceil(motif_window / 2))
     spans = []
     for i in np.where(index_scores_array > threshold)[0]:
-        if  half_motif_window < i < len(seq_1hot)-half_motif_window:
+        if half_motif_window < i < len(seq_1hot) - half_motif_window:
             spans.append(i)
     return spans
 
+
 def permute_spans(seq_1hot, spans, motif_window, shuffle_parameter):
     seq_1hot_mut = seq_1hot.copy()
-    half_motif_window = int(np.ceil(motif_window/2))
+    half_motif_window = int(np.ceil(motif_window / 2))
     for s in spans:
-        start, end = s-half_motif_window, s+half_motif_window
-        seq_1hot_mut[start:end] = akita_utils.dna_utils.permute_seq_k(seq_1hot_mut[start:end], k=shuffle_parameter)
+        start, end = s - half_motif_window, s + half_motif_window
+        seq_1hot_mut[start:end] = akita_utils.dna_utils.permute_seq_k(
+            seq_1hot_mut[start:end], k=shuffle_parameter
+        )
     return seq_1hot_mut
+
 
 def mask_spans(seq_1hot, spans, motif_window):
     seq_1hot_perm = seq_1hot.copy()
-    half_motif_window = int(np.ceil(motif_window/2))
+    half_motif_window = int(np.ceil(motif_window / 2))
     for s in spans:
-        start, end = s-half_motif_window, s+half_motif_window
-        seq_1hot_perm[start : end, :] = 0
+        start, end = s - half_motif_window, s + half_motif_window
+        seq_1hot_perm[start:end, :] = 0
     return seq_1hot_perm
+
 
 def randomise_spans(seq_1hot, spans, motif_window):
     seq_1hot_perm = seq_1hot.copy()
-    half_motif_window = int(np.ceil(motif_window/2))
+    half_motif_window = int(np.ceil(motif_window / 2))
     for s in spans:
-        start, end = s-half_motif_window, s+half_motif_window
-        seq_1hot_perm[start : end] = random_seq_permutation(seq_1hot_perm[start : end])
+        start, end = s - half_motif_window, s + half_motif_window
+        seq_1hot_perm[start:end] = random_seq_permutation(
+            seq_1hot_perm[start:end]
+        )
     return seq_1hot_perm
+
 
 def random_seq_permutation(seq_1hot):
     seq_1hot_perm = seq_1hot.copy()
@@ -157,23 +240,32 @@ def random_seq_permutation(seq_1hot):
     seq_1hot_perm = seq_1hot[random_inds, :].copy()
     return seq_1hot_perm
 
+
 def background_exploration_seqs_gen(seq_coords_df, genome_open, use_span=True):
     motif = akita_utils.format_io.read_jaspar_to_numpy()
-    motif_window = len(motif)-3 #for compartibility ie (19-3=16 which is a multiple of 2,4,8 the shuffle parameters)
+    motif_window = (
+        len(motif) - 3
+    )  # for compartibility ie (19-3=16 which is a multiple of 2,4,8 the shuffle parameters)
     for s in seq_coords_df.itertuples():
-        chrom,start,end = s.locus_specification.split(",")
+        chrom, start, end = s.locus_specification.split(",")
         seq_dna = genome_open.fetch(chrom, int(start), int(end))
         wt_1hot = akita_utils.dna_utils.dna_1hot(seq_dna)
         mutation_method = s.mutation_method
-        spans = generate_spans_start_positions(wt_1hot, motif, s.ctcf_selection_threshold)
+        spans = generate_spans_start_positions(
+            wt_1hot, motif, s.ctcf_selection_threshold
+        )
         if mutation_method == "mask_motif":
             yield mask_spans(wt_1hot, spans, motif_window)
         elif mutation_method == "permute_motif":
-            yield permute_spans(wt_1hot, spans, motif_window, s.shuffle_parameter)
+            yield permute_spans(
+                wt_1hot, spans, motif_window, s.shuffle_parameter
+            )
         elif mutation_method == "randomise_motif":
             yield randomise_spans(wt_1hot, spans, motif_window)
         elif mutation_method == "permute_whole_seq":
-            yield akita_utils.dna_utils.permute_seq_k(wt_1hot, k=s.shuffle_parameter)
+            yield akita_utils.dna_utils.permute_seq_k(
+                wt_1hot, k=s.shuffle_parameter
+            )
         elif mutation_method == "randomise_whole_seq":
             yield random_seq_permutation(wt_1hot)
 
