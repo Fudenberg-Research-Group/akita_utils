fb17fe2306a951aa7b71c51673b793f5bc29f1a6
Paulina Smaruj <smaruj@usc.edu>
2023-05-15 11:17:55 -0700

    writting to h5 files using imported from h5_utils functions, saving tested using toy examples

--- bin/insert_virtual_flanks_experiment/virtual_symmetric_experiment_flanks.py
+++ bin/insert_virtual_flanks_experiment/virtual_symmetric_experiment_flanks.py
@@ -69,12 +69,10 @@ os.environ["OPENBLAS_NUM_THREADS"] = "1"
 import pickle
 import random
 
-import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pysam
-from skimage.measure import block_reduce
 import seaborn as sns
 
 sns.set(style="ticks", font_scale=1.3)
@@ -91,8 +89,9 @@ print(gpus)
 from basenji import seqnn, stream, dna_io
 
 from akita_utils.seq_gens import symmertic_insertion_seqs_gen
-from akita_utils.utils import ut_dense, split_df_equally
-from akita_utils.stats_utils import insul_diamonds_scores
+from akita_utils.utils import split_df_equally
+from akita_utils.h5_utils import initialize_output_h5, write_to_h5_stats_for_prediction
+
 
 ################################################################################
 # main
@@ -249,7 +248,7 @@ def main():
     # read parameters and targets
 
     # read model parameters
-    with open(params_file) as params_open:
+    with open(params_file) as params_open:s
         params = json.load(params_open)
     params_train = params["train"]
     params_model = params["model"]
@@ -278,7 +277,7 @@ def main():
     # dummy target info
     if options.targets_file is None:
         num_targets = seqnn_model.num_targets()
-        target_ids = ["t%d" % ti for ti in range(num_targets)]      # checkpoint? to be sure that the langth of given targets_file is compatibile with the requested head?
+        target_ids = [ti for ti in range(num_targets)]      # checkpoint? to be sure that the langth of given targets_file is compatibile with the requested head?
         target_labels = [""] * len(target_ids)
 
     #################################################################
@@ -318,13 +317,12 @@ def main():
             
     #################################################################
     # setup output
-
+    
     scd_out = initialize_output_h5(
         options.out_dir,
-        options.scd_stats,
         seq_coords_df,
+        options.scd_stats,
         target_ids,
-        target_labels,
         head_index,
         model_index
     )
@@ -338,12 +336,12 @@ def main():
     preds_stream = stream.PredStreamGen(
         seqnn_model, symmertic_insertion_seqs_gen(seq_coords_df, background_seqs, genome_open), batch_size
     )
-    
+            
     for exp in range(num_experiments):
         # get predictions
         preds = preds_stream[exp]
-        # process SNP
-        write_snp(
+        # save stat metrics for each prediction
+        write_to_h5_stats_for_prediction(
             preds,
             scd_out,
             exp,
@@ -353,105 +351,12 @@ def main():
             options.scd_stats,
             plot_dir,
             options.plot_lim_min,
-            options.plot_freq,
+            options.plot_freq
         )
     
     genome_open.close()
     scd_out.close()
-
-
-def initialize_output_h5(out_dir, scd_stats, seq_coords_df, target_ids, target_labels, head_index, model_index):
-    """Initialize an output HDF5 file for SCD stats."""
-
-    num_targets = len(target_ids)
-    num_experiments = len(seq_coords_df)
-    scd_out = h5py.File("%s/scd.h5" % out_dir, "w")
-    seq_coords_df_dtypes = seq_coords_df.dtypes
-
-    for key in seq_coords_df:
-        if seq_coords_df_dtypes[key] is np.dtype("O"):
-            scd_out.create_dataset(key, data=seq_coords_df[key].values.astype("S"))
-        else:
-            scd_out.create_dataset(key, data=seq_coords_df[key])
-
-    # initialize scd stats
-    for scd_stat in scd_stats:
         
-        if scd_stat in seq_coords_df.keys():
-            raise KeyError("check input tsv for clashing score name")
-        
-        for target_ind in range(num_targets):
-            scd_out.create_dataset(
-                f"{scd_stat}_h{head_index}_m{model_index}_t{target_ind}",
-                shape=(num_experiments,),
-                dtype="float16",
-                compression=None,
-            )
-
-    return scd_out
-
-
-def write_snp(
-    ref_preds,
-    scd_out,
-    si,
-    head_index,
-    model_index,
-    diagonal_offset,
-    scd_stats=["SCD"],
-    plot_dir=None,
-    plot_lim_min=0.1,
-    plot_freq=100,
-):
-    """Write SNP predictions to HDF."""
-    
-    # increase dtype
-    ref_preds = ref_preds.astype("float32")
-        
-    # compare reference to alternative via mean subtraction
-    if "SCD" in scd_stats:
-        # sum of squared diffs
-        sd2_preds = np.sqrt((ref_preds**2).sum(axis=0))
-        for target_ind in range(ref_preds.shape[1]):
-            scd_out[f"SCD_h{head_index}_m{model_index}_t{target_ind}"][si] = sd2_preds[target_ind].astype("float16")
-
-    if np.any((["INS" in i for i in scd_stats])):
-        ref_map = ut_dense(ref_preds, diagonal_offset)
-        for stat in scd_stats:
-            if "INS" in stat:
-                insul_window = int(stat.split("-")[1])
-                
-                for target_ind in range(ref_preds.shape[1]):
-                    scd_out[f"{stat}_h{head_index}_m{model_index}_t{target_ind}"][si] = insul_diamonds_scores(ref_map, window=insul_window)[target_ind].astype("float16")
-
-    if (plot_dir is not None) and (np.mod(si, plot_freq) == 0):
-        print("plotting ", si)
-        # convert back to dense
-        ref_map = ut_dense(ref_preds, diagonal_offset)
-        _, axs = plt.subplots(1, ref_preds.shape[-1], figsize=(24, 4))
-        for ti in range(ref_preds.shape[-1]):
-            ref_map_ti = ref_map[..., ti]
-            # TEMP: reduce resolution
-            ref_map_ti = block_reduce(ref_map_ti, (2, 2), np.mean)
-            vmin = min(ref_map_ti.min(), ref_map_ti.min())
-            vmax = max(ref_map_ti.max(), ref_map_ti.max())
-            vmin = min(-plot_lim_min, vmin)
-            vmax = max(plot_lim_min, vmax)
-            sns.heatmap(
-                ref_map_ti,
-                ax=axs[ti],
-                center=0,
-                vmin=vmin,
-                vmax=vmax,
-                cmap="RdBu_r",
-                xticklabels=False,
-                yticklabels=False,
-            )
-
-        plt.tight_layout()
-        plt.savefig("%s/s%d.pdf" % (plot_dir, si))
-        plt.close()
-
 ################################################################################
 # __main__
 ################################################################################
