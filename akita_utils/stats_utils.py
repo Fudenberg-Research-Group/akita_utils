c9e21af9c38a9b1b1037d838b02e53b5e8428a36
Paulina Smaruj <smaruj@usc.edu>
2023-05-17 16:06:08 -0700

    updated, but temporary, version of SCD-score functions

--- akita_utils/stats_utils.py
+++ akita_utils/stats_utils.py
@@ -22,11 +22,49 @@ def insul_diamonds_scores(mats, window=10):
     return scores
 
 
-def scd_target_matrix(prediction_matrix):
-    return np.sqrt((prediction_matrix**2).sum(axis=0))
+def scd_target_matrix(prediction_matrix, reference_matrix=None):
+    """
+    Calculates SCD score for a multiple-target prediction matrix.
+    If reference_matrix is not given, it is assumed that an insertion-into-background
+    experiment has been performed so reference values are close to 0.
+    
+    Parameters
+    ------------
+    prediction_matrix : numpy array
+        Array with Akita predictions; size = (130305, num_targets)
+    reference_matrix : numpy array
+        Array with Akita predictions for a reference sequence; size = (130305, num_targets)
+        
+    Returns
+    ---------
+    num_targets-long vector with SCD score calculated for eacg target.
+    """
+    if reference_matrix == None:
+        return np.sqrt((prediction_matrix**2).sum(axis=0))
+    else:
+        return np.sqrt(((prediction_matrix-reference_matrix)**2).sum(axis=0))
 
 
-def scd_single_pred(prediction_vector):
-    return np.sqrt(prediction_vector**2)
+def scd_single_pred(prediction_vector, reference_vector=None):
+    """
+    Calculates SCD score for a single-target prediction vector.
+    If reference_vector is not given, it is assumed that an insertion-into-background
+    experiment has been performed so reference values are close to 0.
+    
+    Parameters
+    ------------
+    prediction_vector : numpy array
+        Vector with Akita predictions; size = (130305,)
+    reference_vector : numpy array
+        Vector with Akita predictions for a reference sequence; size = (130305,)
+        
+    Returns
+    ---------
+    A float-type SCD score.
+    """
+    if reference_vector == None:
+        return np.sqrt(prediction_vector**2)
+    else:
+        return np.sqrt((prediction_vector-reference_vector)**2)
 
                    
