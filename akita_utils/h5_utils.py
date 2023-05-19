444b353fda0876ec51ad9a3aea6273af1155f48c
Paulina Smaruj <smaruj@usc.edu>
2023-05-18 17:18:44 -0700

    implementation of proposed changes: calculate_scores, saving entire maps and a separated plotting function

--- akita_utils/h5_utils.py
+++ akita_utils/h5_utils.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from akita_utils.utils import ut_dense
-from akita_utils.stats_utils import insul_diamonds_scores, scd_target_matrix
+from akita_utils.stats_utils import calculate_scores
 
 
 def initialize_output_h5(
@@ -30,22 +30,22 @@ def initialize_output_h5(
 
     Returns
     ---------
-    scd_out : h5py object
+    h5_outfile : h5py object
         An initialized h5 file.
     """
 
     num_experiments = len(seq_coords_df)
 
-    scd_out = h5py.File("%s/scd.h5" % out_dir, "w")
+    h5_outfile = h5py.File(f"%s/stat_summary_h{head_index}_m{model_index}.h5" % out_dir, "w")
     seq_coords_df_dtypes = seq_coords_df.dtypes
 
     for key in seq_coords_df:
         if seq_coords_df_dtypes[key] is np.dtype("O"):
-            scd_out.create_dataset(
+            h5_outfile.create_dataset(
                 key, data=seq_coords_df[key].values.astype("S")
             )
         else:
-            scd_out.create_dataset(key, data=seq_coords_df[key])
+            h5_outfile.create_dataset(key, data=seq_coords_df[key])
 
     # initialize keys for statistical metrics collection
     for stat_metric in stat_metrics:
@@ -54,7 +54,7 @@ def initialize_output_h5(
             raise KeyError("check input tsv for clashing score name")
 
         for target_index in target_ids:
-            scd_out.create_dataset(
+            h5_outfile.create_dataset(
                 f"{stat_metric}_h{head_index}_m{model_index}_t{target_index}",
                 shape=(num_experiments,),
                 dtype="float16",
@@ -64,17 +64,14 @@ def initialize_output_h5(
     return scd_out
 
 
-def write_to_h5_stats_for_prediction(
+def write_stat_metrics_to_h5(
     prediction_matrix,
-    scd_out,
+    h5_outfile,
     experiment_index,
     head_index,
     model_index,
     diagonal_offset=2,
-    stat_metrics=["SCD"],
-    plot_dir=None,
-    plot_lim_min=0.1,
-    plot_freq=100,
+    stat_metrics=["SCD"]
 ):
     """
     Writes to an h5 file saving statistical metrics calculated from Akita's predicftions.
@@ -83,7 +80,7 @@ def write_to_h5_stats_for_prediction(
     ------------
     prediction_matrix : numpy matrix
         Matrix collecting Akita's predictions.
-    scd_out : h5py object
+    h5_outfile : h5py object
         An initialized h5 file.
     experiment_index : int
         Index identifying one experiment.
@@ -106,46 +103,120 @@ def write_to_h5_stats_for_prediction(
 
     # increase dtype
     prediction_matrix = prediction_matrix.astype("float32")
-
-    # saving
-    if "SCD" in stat_metrics:
-        SCDs = scd_target_matrix(prediction_matrix)
-        for target_ind in range(prediction_matrix.shape[1]):
-            scd_out[f"SCD_h{head_index}_m{model_index}_t{target_ind}"][
+    
+    # convert prediction vectors to maps
+    map_matrix = ut_dense(prediction_matrix, diagonal_offset)
+    
+    # getting desired scores
+    # TODO: reference_map_matrix may not be None if the experiments are done in the genomic context,
+    # it should be adjusted in the future
+    scores = calculate_scores(stat_metrics, map_matrix, reference_map_matrix=None)
+    
+    for score in scores:
+        for target_index in range(prediction_matrix.shape[1]):
+            h5_outfile[f"{score}_h{head_index}_m{model_index}_t{target_index}"][
                 experiment_index
-            ] = SCDs[target_ind].astype("float16")
+            ] = score[target_ind].astype("float16")
 
-    if np.any((["INS" in i for i in stat_metrics])):
-        ref_map = ut_dense(prediction_matrix, diagonal_offset)
-        for stat in stat_metrics:
-            if "INS" in stat:
-                insul_window = int(stat.split("-")[1])
-                for target_ind in range(prediction_matrix.shape[1]):
-                    scd_out[
-                        f"{stat}_h{head_index}_m{model_index}_t{target_ind}"
-                    ][experiment_index] = insul_diamonds_scores(
-                        ref_map, window=insul_window
-                    )[
-                        target_ind
-                    ].astype(
-                        "float16"
-                    )
 
+def write_maps_to_h5(
+    prediction_matrix,
+    h5_outfile,
+    experiment_index,
+    head_index,
+    model_index,
+    diagonal_offset=2,
+    plot_dir=None
+):
+    """
+    Writes entire maps to an h5 file.
+
+    Parameters
+    ------------
+    prediction_matrix : numpy matrix
+        Matrix collecting Akita's predictions.
+    h5_outfile : h5py object
+        An initialized h5 file.
+    experiment_index : int
+        Index identifying one experiment.
+    head_index : int
+        Head index used to get a prediction (Mouse: head_index=1; Human: head_index=0).
+    model_index : int
+        Index of one of 8 models that has been used to make predictions (an index between 0 and 7).
+    diagonal_offset : int
+        Number of diagonals that are added as zeros in the conversion.
+        Typically 2 diagonals are ignored in Hi-C data processing.
+    plot_dir : str
+        Path to the desired location of the output plots (plots will not be saved if plot_dir == None).
+    """
+    
+    # increase dtype
+    prediction_matrix = prediction_matrix.astype("float32")
+    
+    # convert prediction vectors to maps
+    map_matrix = ut_dense(prediction_matrix, diagonal_offset)
+    
+    for target_index in range(prediction_matrix.shape[1]):
+        h5_outfile[f"e{experiment_index}_h{head_index}_m{model_index}_t{target_index}"] = map_matrix[:, :, target_index]
+        
+            
+# TODO: this function should be moved somewhere else - where?
+            
+def save_maps(
+    prediction_matrix,
+    experiment_index,
+    head_index,
+    model_index,
+    diagonal_offset=2,
+    plot_dir=None,
+    plot_lim_min=0.1,
+    plot_freq=100
+):
+    
+    """
+    Writes to an h5 file saving statistical metrics calculated from Akita's predicftions.
+
+    Parameters
+    ------------
+    prediction_matrix : numpy matrix
+        Matrix collecting Akita's predictions.
+    experiment_index : int
+        Index identifying one experiment.
+    head_index : int
+        Head index used to get a prediction (Mouse: head_index=1; Human: head_index=0).
+    model_index : int
+        Index of one of 8 models that has been used to make predictions (an index between 0 and 7).
+    diagonal_offset : int
+        Number of diagonals that are added as zeros in the conversion.
+        Typically 2 diagonals are ignored in Hi-C data processing.
+    plot_dir : str
+        Path to the desired location of the output plots (plots will not be saved if plot_dir == None).
+    plot_lim_min : float
+        Negative minimum and positive maximum values that will be used to plot maps.
+    plot_freq : int
+        A plot of one out of plot_freq number of predictions is saved.
+    """
+            
     if (plot_dir is not None) and (np.mod(experiment_index, plot_freq) == 0):
         print("plotting prediction: ", experiment_index)
 
-        ref_map = ut_dense(prediction_matrix, diagonal_offset)
+        # increase dtype
+        prediction_matrix = prediction_matrix.astype("float32")
+
+        # convert prediction vectors to maps
+        map_matrix = ut_dense(prediction_matrix, diagonal_offset)
+
         _, axs = plt.subplots(1, prediction_matrix.shape[-1], figsize=(24, 4))
 
         for target_index in range(prediction_matrix.shape[-1]):
-            ref_map_target = ref_map[..., target_index]
-            ref_map_target = block_reduce(ref_map_target, (2, 2), np.mean)
-            vmin = min(ref_map_target.min(), ref_map_target.min())
-            vmax = max(ref_map_target.max(), ref_map_target.max())
+            map_target = map_matrix[..., target_index]
+            map_target = block_reduce(map_target, (2, 2), np.mean)
+            vmin = min(map_target.min(), map_target.min())
+            vmax = max(map_target.max(), map_target.max())
             vmin = min(-plot_lim_min, vmin)
             vmax = max(plot_lim_min, vmax)
             sns.heatmap(
-                ref_map_target,
+                map_target,
                 ax=axs[target_index],
                 center=0,
                 vmin=vmin,
@@ -156,5 +227,10 @@ def write_to_h5_stats_for_prediction(
             )
 
         plt.tight_layout()
-        plt.savefig("%s/s%d.pdf" % (plot_dir, experiment_index))
+        
+        # TODO: we can save SCD or other scores in the name of plots
+        # we should let user change which metric
+        # such that they can sort plots by name
+        
+        plt.savefig(f"e{experiment_index}_h{head_index}_m{model_index}_t{target_index}.pdf")
         plt.close()
