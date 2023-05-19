ccc5b21c11cb82062f0399c2565579ac8151cad6
Kamulegeya-Fahad <kamulege@discovery1.usc.edu>
2023-05-19 11:49:42 -0700

    removed stats from akita motif

--- bin/disrupt_genomic_boundary_ctcfs/akita_motif_scd.py
+++ bin/disrupt_genomic_boundary_ctcfs/akita_motif_scd.py
@@ -31,6 +31,7 @@ import pandas as pd
 import pysam
 from skimage.measure import block_reduce
 import seaborn as sns
+from akita_utils.stats_utils import insul_diamonds_scores
 
 sns.set(style="ticks", font_scale=1.3)
 
@@ -351,26 +352,6 @@ def initialize_output_h5(out_dir, scd_stats, seq_coords_df, target_ids, target_l
             )
 
     return scd_out
-
-
-def _insul_diamond_central(mat, window=10):
-    """calculate insulation in a diamond around the central pixel"""
-    N = mat.shape[0]
-    if window > N // 2:
-        raise ValueError("window cannot be larger than matrix")
-    mid = N // 2
-    lo = max(0, mid + 1 - window)
-    hi = min(mid + window, N)
-    score = np.nanmean(mat[lo : (mid + 1), mid:hi])
-    return score
-
-
-def insul_diamonds_scores(mats, window=10):
-    num_targets = mats.shape[-1]
-    scores = np.zeros((num_targets,))
-    for ti in range(num_targets):
-        scores[ti] = _insul_diamond_central(mats[:, :, ti], window=window)
-    return scores
 
 
 def write_snp(
