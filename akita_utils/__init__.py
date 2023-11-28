4fa931522d8cd688b6bc4ae88a1b44514c0759a9
Paulina Smaruj <smaruj@usc.edu>
2023-11-28 15:23:02 -0800

    adding all the new modules

--- akita_utils/__init__.py
+++ akita_utils/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["utils", "background_utils", "dna_utils", "format_io", "seq_gens", "stats_utils", "tsv_gen_utils"]
+__all__ = ["utils", "background_utils", "dna_utils", "h5_utils", "slurm_gf", "format_io", "seq_gens", "stats_utils", "tsv_gen_utils"]
